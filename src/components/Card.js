73df6f03c55de9174b4ca98bc2775fd9037cf57c
VanGamo <vangamo.beta@gmail.com>
2020-07-22 00:04:18 +0200

    Converting Card class component to functional component.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import '../stylesheets/Card.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <article className="card">
-        <div className="greypart">
-          <div className="photo">
-            <img src="none" alt="IMG" />
-          </div>
-          <span className="id">
-            ID / 2
-          </span>
+const App = (props) => {
+  const pkId = props.uri.substr(-2,1);
+  const pokemonData = {
+    id: pkId,
+    name: '',
+    imageURI: '',
+    kind: [],
+    evolvesFrom: null
+  };
+
+  const kindList = pokemonData.kind.map( kind => (
+    <span>{kind}</span>
+  ));
+
+  return (
+    <article className="card">
+      <div className="greypart">
+        <div className="photo">
+          <img src="none" alt="IMG" />
         </div>
-        <div className="whitepart">
-          <div className="name">{this.props.id}</div>
-          <div className="kind"><span className="">kind1</span><span className="">kind2</span></div>
-          <div className="evolution">
-            <div className="ttile">Evoluciona de:</div>
-            <div className="parent">EVOL</div>
-          </div>
+        <span className="id">
+          ID / {pokemonData.id}
+        </span>
+      </div>
+      <div className="whitepart">
+        <div className="name">{pokemonData.name}</div>
+        <div className="kind">{kindList}</div>
+        <div className="evolution">
+          <div className="ttile">Evoluciona de:</div>
+          <div className="parent">EVOL</div>
         </div>
-      </article>
-    );
-  }
+      </div>
+    </article>
+  );
 }
+
 
 export default App;
