f760983fa3c3f5cc173f3ac7b9555d16218896b5
VanGamo <vangamo.beta@gmail.com>
2020-07-22 00:09:54 +0200

    Fetch each Pokemon data and print it into the card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonService from '../services/PokemonService';
 import '../stylesheets/Card.scss';
 
-const App = (props) => {
-  const pkId = props.uri.substr(-2,1);
-  const pokemonData = {
-    id: pkId,
+function capitalize( text ) {
+  return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
+}
+
+const useFetchPokemon = (id) => {
+  const [pokemonData, setPokemonData] = useState({
+    id: id,
     name: '',
     imageURI: '',
     kind: [],
     evolvesFrom: null
-  };
+  });
+
+  useEffect(() => {
+    PokemonService.getInstance()
+      .getPokemon( id )
+      .then( data => {
+        setPokemonData({
+          ...data,
+          name: capitalize( data.name )
+        });
+      })
+      .catch(error => {console.error(error);});
+  });
+
+  return pokemonData;
+}
+
+const App = (props) => {
+  const pkId = props.uri.substr(-2,1);
+  const pokemonData = useFetchPokemon(pkId);
 
   const kindList = pokemonData.kind.map( kind => (
     <span>{kind}</span>
@@ -19,7 +42,7 @@ const App = (props) => {
     <article className="card">
       <div className="greypart">
         <div className="photo">
-          <img src="none" alt="IMG" />
+          <img src={pokemonData.image} alt={'Imagen de ' + pokemonData.name} />
         </div>
         <span className="id">
           ID / {pokemonData.id}
@@ -28,14 +51,15 @@ const App = (props) => {
       <div className="whitepart">
         <div className="name">{pokemonData.name}</div>
         <div className="kind">{kindList}</div>
-        <div className="evolution">
-          <div className="ttile">Evoluciona de:</div>
-          <div className="parent">EVOL</div>
-        </div>
+        {!!pokemonData.evolvesFrom && (
+          <div className="evolution">
+            <div className="ttile">Evoluciona de:</div>
+            <div className="parent">EVOL</div>
+          </div>
+        )}
       </div>
     </article>
   );
 }
-
 
 export default App;
--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -25,6 +25,24 @@ class PokemonService {
         return filteredData;
       });
   }
+
+  getPokemon( id ) {
+    return fetch( BASE_URL + `${id}/` )
+      .then( response => response.json() )
+      .then( data => {
+        const sortedTypes = data.types.sort( (first, second) => first.slot > second.slot );
+        const kinds = sortedTypes.map( eachType => eachType.type.name );
+
+        const pokemonData = {
+          id:    data.id,
+          name:  data.name,
+          image: data.sprites.front_default,
+          kind:  kinds
+        };
+
+        return pokemonData;
+      })
+  }
 }
 
 export default PokemonService;
