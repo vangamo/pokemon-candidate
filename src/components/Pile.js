b00df12c13e1854c215f40a95bb8270f78032912
VanGamo <vangamo.beta@gmail.com>
2020-07-22 12:09:20 +0200

    Change Pile component to a functional component.

--- src/components/Pile.js
+++ src/components/Pile.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
 import '../stylesheets/Pile.scss';
 
-class Pile extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Pile = (props) => {
+  const cards = props.pokemonList.map( pokemon => (
+    <Card key={pokemon.uri} uri={pokemon.uri} name={pokemon.name}></Card>
+  ));
 
-  render() {
-    const cards = this.props.pokemonList.map( pokemon => (
-      <Card key={pokemon.uri} uri={pokemon.uri} name={pokemon.name}></Card>
-    ));
-
-    return (
-      <section className="pile">
-        {cards}
-      </section>
-    );
-  }
-}
+  return (
+    <section className="pile">
+      {cards}
+    </section>
+  );
+};
 
 export default Pile;
