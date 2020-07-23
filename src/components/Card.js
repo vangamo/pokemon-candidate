77295ec3b1fee59defab7aa00f6654c5b909eed4
VanGamo <vangamo.beta@gmail.com>
2020-07-23 18:54:51 +0200

    Refactoring Cards and Details to eliminate duplicate code. First, converting both functional components into class components.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PokemonService from '../services/PokemonService';
 import '../stylesheets/Card.scss';
 
@@ -16,34 +16,52 @@ function capitalize( text ) {
   return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
 }
 
-
 /**
- * Hook to double fetch the Pokemon data using PokemonService.
- * It has to take Pokemon data from two endpoints:
- *  - /pokemon/         For the sprite image and kind of the Pokemon
- *  - /pokemon-species/ For the evolution data
+ * Prints a single card with the Pokemon data.
+ * It has two parts:
+ *  - Grey part with the image and ID.
+ *  - White part with the name, type and evolves from info.
  * 
- * @param {int} id It's the numeric ID (set by API) of the Pokemon to fetch.
  */
 
-const useFetchPokemon = (id, name) => {
-  const [pokemonData, setPokemonData] = useState({
-    id: id,
-    name: capitalize(name),
-    imageURI: '',
-    kind: [],
-    evolvesFrom: null
-  });
+class Card extends Component {
+  constructor( props ) {
+    super( props );
 
-  useEffect(() => {
-    let isComponentMounted = true;
+    this.state = {
+      id: props.id,
+      name: capitalize(props.name),
+      imageURI: '',
+      kind: [],
+      evolvesFrom: null
+    };
+
+    this.isComponentMounted = true;
+
+    this.handleClick = this.handleClick.bind( this );
+  }
+
+  /**
+   * Use DidMount to double fetch the Pokemon data using PokemonService.
+   * It has to take Pokemon data from two endpoints:
+   *  - /pokemon/         For the sprite image and kind of the Pokemon
+   *  - /pokemon-species/ For the evolution data
+   * This data is stored in the state.
+   * This function uses a hack inside the fetch process to prevent memory
+   * leaks due to lost information when the fetch response arrives too late.
+   * It checks if the component has been unmount before assign the data.
+   *
+   */
+
+  componentDidMount() {
+    const pokemonId = this.props.id;
 
     PokemonService.getInstance()
-      .getPokemonData( id )
+      .getPokemonData( pokemonId )
       .then( data => {
-        if( isComponentMounted ) {
-          setPokemonData({
-            ...pokemonData,
+        if( this.isComponentMounted ) {
+          this.setState({
+            ...this.state,
             ...data,
             name: capitalize( data.name )
           });
@@ -51,79 +69,79 @@ const useFetchPokemon = (id, name) => {
       })
       .catch(error => {console.error(error);});
 
-      PokemonService.getInstance()
-        .getPokemonEvolution( id )
-        .then( data => {
-          if( isComponentMounted ) {
-            setPokemonData({
-              ...pokemonData,
-              ...data
-            });
-          }
-        })
-        .catch(error => {console.error(error);});
-
-      return () => { isComponentMounted = false; };
-  });
-
-  return pokemonData;
-}
-
-
-/**
- * Prints a single card with the Pokemon data.
- * It has two parts:
- *  - Grey part with the image and ID.
- *  - White part with the name, type and evolves from info.
- * 
- * @param {*} props 
- */
-
-const Card = (props) => {
-  const pokemonData = useFetchPokemon( props.id, props.name );
-
-  const history = useHistory();
-
-  function handleClick() {
-    history.push( `/details/${props.id}-${props.name}`  );
+    PokemonService.getInstance()
+      .getPokemonEvolution( pokemonId )
+      .then( data => {
+        if( this.isComponentMounted ) {
+          this.setState({
+            ...this.state,
+            ...data
+          });
+        }
+      })
+      .catch(error => {console.error(error);});
   }
 
-  const kindList = pokemonData.kind.map( kind => (
-    <span key={kind}>{kind}</span>
-  ));
+  /**
+   * Completes the hack used in the fetch functions into componentDidMount().
+   * 
+   */
 
-  return (
-    <article className="card" onClick={handleClick}>
-      <div className="greypart">
-        <div className="photo">
-          <img src={pokemonData.imageURI} alt={ (pokemonData.imageURI==='') ? `Cargando imagen de ${pokemonData.name}` : `Imagen de ${pokemonData.name}`} />
-        </div>
-        <span className="id">
-          ID / {pokemonData.id}
-        </span>
-      </div>
-      <div className="whitepart">
-        <div className="name">{pokemonData.name}</div>
-        <div className="kind">{kindList}</div>
-        {!!pokemonData.evolvesFrom && (
-          <div className="evolution">
-            <div className="ttile">Evoluciona de:</div>
-            <div className="parent">EVOL</div>
+   componentWillUnmount() {
+    this.isComponentMounted = false;
+  }
+
+  /**
+   * When the user clicks on the card this function loads the details page.
+   */
+
+  handleClick() {
+    this.props.history.push( `/details/${this.props.id}-${this.props.name}`  );
+  }
+
+  /**
+   * Prints data.
+   *
+   */
+
+  render() {
+    const pokemonData = this.state;
+    const kindList    = pokemonData.kind.map( kind => (
+      <span key={kind}>{kind}</span>
+    ));
+  
+    return (
+      <article className="card" onClick={this.handleClick}>
+        <div className="greypart">
+          <div className="photo">
+            <img src={pokemonData.imageURI} alt={ (pokemonData.imageURI==='') ? `Cargando imagen de ${pokemonData.name}` : `Imagen de ${pokemonData.name}`} />
           </div>
-        )}
-      </div>
-    </article>
-  );
+          <span className="id">
+            ID / {pokemonData.id}
+          </span>
+        </div>
+        <div className="whitepart">
+          <div className="name">{pokemonData.name}</div>
+          <div className="kind">{kindList}</div>
+          {!!pokemonData.evolvesFrom && (
+            <div className="evolution">
+              <div className="ttile">Evoluciona de:</div>
+              <div className="parent">EVOL</div>
+            </div>
+          )}
+        </div>
+      </article>
+    );
+  }
 }
 
 Card.propTypes = {
-  id:   PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
-};
+    id:   PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  };
 
 Card.defaultProps = {
   // id,name isRequired
 };
 
-
-export default Card;
+export default withRouter( Card );
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PokemonService from '../services/PokemonService';
 import '../stylesheets/Details.scss';
 
@@ -16,34 +16,52 @@ function capitalize( text ) {
   return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
 }
 
-
 /**
- * Hook to double fetch the Pokemon data using PokemonService.
- * It has to take Pokemon data from two endpoints:
- *  - /pokemon/         For the sprite image and kind of the Pokemon
- *  - /pokemon-species/ For the evolution data
+ * Prints a single card with the Pokemon details filling all the with of the page.
+ * It has two parts:
+ *  - Grey part with the image and ID.
+ *  - White part with the name, type and evolves from info.
  * 
- * @param {int} id It's the numeric ID (set by API) of the Pokemon to fetch.
  */
 
-const useFetchPokemon = (id, name) => {
-  const [pokemonData, setPokemonData] = useState({
-    id: id,
-    name: capitalize(name),
-    imageURI: '',
-    kind: [],
-    evolvesFrom: null
-  });
+class Details extends Component {
+  constructor( props ) {
+    super(props);
 
-  useEffect(() => {
-    let isComponentMounted = true;
+    const [pokemonId, pokemonName] = props.match.params.key.split('-');
 
+    this.state = {
+      id: pokemonId,
+      name: capitalize(pokemonName),
+      imageURI: '',
+      kind: [],
+      evolvesFrom: null
+    };
+
+    this.isComponentMounted = true;
+
+    this.handleClickGoBack = this.handleClickGoBack.bind( this );
+  }
+
+  /**
+   * Use DidMount to double fetch the Pokemon data using PokemonService.
+   * It has to take Pokemon data from two endpoints:
+   *  - /pokemon/         For the sprite image and kind of the Pokemon
+   *  - /pokemon-species/ For the evolution data
+   * This data is stored in the state.
+   * This function uses a hack inside the fetch process to prevent memory
+   * leaks due to lost information when the fetch response arrives too late.
+   * It checks if the component has been unmount before assign the data.
+   *
+   */
+
+  componentDidMount() {
     PokemonService.getInstance()
-      .getPokemonData( id )
+      .getPokemonData( this.state.id )
       .then( data => {
-        if( isComponentMounted ) {
-          setPokemonData({
-            ...pokemonData,
+        if( this.isComponentMounted ) {
+          this.setState({
+            ...this.state,
             ...data,
             name: capitalize( data.name )
           });
@@ -51,75 +69,75 @@ const useFetchPokemon = (id, name) => {
       })
       .catch(error => {console.error(error);});
 
-      PokemonService.getInstance()
-        .getPokemonEvolution( id )
-        .then( data => {
-          if( isComponentMounted ) {
-            setPokemonData({
-              ...pokemonData,
-              ...data
-            });
-          }
-        })
-        .catch(error => {console.error(error);});
-
-      return () => { isComponentMounted = false; };
-  });
-
-  return pokemonData;
-}
-
-
-/**
- * Prints a single card with the Pokemon details filling all the with of the page.
- * It has two parts:
- *  - Grey part with the image and ID.
- *  - White part with the name, type and evolves from info.
- * 
- * @param {*} props 
- */
-
-const Details = (props) => {
-  const [pokemonId, pokemonName] = props.match.params.key.split('-');
-  const pokemonData = useFetchPokemon( pokemonId, pokemonName );
-
-  const history = useHistory();
-
-  function handleClickGoBack() {
-    history.push( '/' );
+    PokemonService.getInstance()
+      .getPokemonEvolution( this.state.id )
+      .then( data => {
+        if( this.isComponentMounted ) {
+          this.setState({
+            ...this.state,
+            ...data
+          });
+        }
+      })
+      .catch(error => {console.error(error);});
   }
 
-  const kindList = pokemonData.kind.map( kind => (
-    <span key={kind}>{kind}</span>
-  ));
+  /**
+   * Completes the hack used in the fetch functions into componentDidMount().
+   * 
+   */
 
-  return (
-    <section className="details">
-      <div className="breadcrumb">
-        <input type="button" className="back" value="Volver a la lista" onClick={handleClickGoBack} />
-      </div>
-      <article className="card big">
-        <div className="greypart">
-          <div className="photo">
-            <img src={pokemonData.imageURI} alt={ (pokemonData.imageURI==='') ? `Cargando imagen de ${pokemonData.name}` : `Imagen de ${pokemonData.name}`} />
-          </div>
-          <span className="id">
-            ID / {pokemonData.id}
-          </span>
+  componentWillUnmount() {
+    this.isComponentMounted = false;
+  }
+
+  /**
+   * When the user clicks on the go back button this function loads the search page.
+   */
+
+  handleClickGoBack() {
+    this.props.history.push( '/' );
+  }
+
+  /**
+   * Prints data.
+   *
+   */
+
+  render() {
+    const pokemonData = this.state;
+    const kindList    = pokemonData.kind.map( kind => (
+      <span key={kind}>{kind}</span>
+    ));
+
+    return (
+      <section className="details">
+        <div className="breadcrumb">
+          <input type="button" className="back" value="Volver a la lista" onClick={this.handleClickGoBack} />
         </div>
-        <div className="whitepart">
-          <div className="name">{pokemonData.name}</div>
-          <div className="kind">{kindList}</div>
-          {!!pokemonData.evolvesFrom && (
-            <div className="evolution">
-              <div className="ttile">Evoluciona de:</div>
-              <div className="parent">EVOL</div>
+        <article className="card big">
+          <div className="greypart">
+            <div className="photo">
+              <img src={pokemonData.imageURI} alt={ (pokemonData.imageURI==='') ? `Cargando imagen de ${pokemonData.name}` : `Imagen de ${pokemonData.name}`} />
             </div>
-          )}
-        </div>
-      </article>
-    </section>
-  );
+            <span className="id">
+              ID / {pokemonData.id}
+            </span>
+          </div>
+          <div className="whitepart">
+            <div className="name">{pokemonData.name}</div>
+            <div className="kind">{kindList}</div>
+            {!!pokemonData.evolvesFrom && (
+              <div className="evolution">
+                <div className="ttile">Evoluciona de:</div>
+                <div className="parent">EVOL</div>
+              </div>
+            )}
+          </div>
+        </article>
+      </section>
+    );
+  }
 }
 
 Details.propTypes = {
@@ -130,4 +148,4 @@ Details.defaultProps = {
   // match isRequired
 };
 
-export default Details;
+export default withRouter( Details );
