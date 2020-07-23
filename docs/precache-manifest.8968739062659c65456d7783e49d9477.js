5afc363d6983dde306300d340752421f1300fa7f
VanGamo <vangamo.beta@gmail.com>
2020-07-23 13:54:50 +0200

    First version. Production ready! :D

---
+++ docs/precache-manifest.8968739062659c65456d7783e49d9477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4f5e9b3e656892b489576149403b03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89991f981ff8ef9acd9a",
+    "url": "./static/css/main.f8e34228.chunk.css"
+  },
+  {
+    "revision": "975d7d2a57910d5f4ab9",
+    "url": "./static/js/2.d36e90f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d36e90f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89991f981ff8ef9acd9a",
+    "url": "./static/js/main.bbe22937.chunk.js"
+  },
+  {
+    "revision": "b6b9fcbe41b2fc9554bb",
+    "url": "./static/js/runtime-main.8a57af4e.js"
+  },
+  {
+    "revision": "6cc7b065464b4e96433dfc406c2a6d71",
+    "url": "./static/media/pika-error.6cc7b065.gif"
+  }
+]);
